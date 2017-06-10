c8b99ba11b1ba0e24b4e3e34b33c885ddf18d4f3
txgcwm <txgcwm@163.com>
2017-06-10 16:01:17 +0800

    write file

--- shdl/test/TestStreamDownload.cpp
+++ shdl/test/TestStreamDownload.cpp
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <string>
+// #include <iostream>
+// #include <fstream>
 
 #include "shdl/IStreamDownload.h"
 
@@ -9,23 +11,39 @@
 class CTestStreamDownload : public Shdl::IDataCallback
 {
 public:
-	CTestStreamDownload();
+	CTestStreamDownload(std::string filename);
 	~CTestStreamDownload();
 
 	virtual void HttpDlData(const void *context, const void *data, const size_t size);
+
+private:
+	FILE* m_file;
 };
 
-CTestStreamDownload::CTestStreamDownload()
+CTestStreamDownload::CTestStreamDownload(std::string filename)
+: m_file(NULL)
 {
+	if(filename.size() > 0) {
+		printf("filename(%s)\n", filename.c_str());
+		m_file = fopen(filename.c_str(), "w+");
+		if(m_file == NULL) {
+			printf("can not open file\n");
+		}
+	}
 }
 
 CTestStreamDownload::~CTestStreamDownload()
 {
+	fclose(m_file);
+	m_file = NULL;
 }
 
 void CTestStreamDownload::HttpDlData(const void *context, const void *data, const size_t size)
 {
-	printf("%lu\n", size);
+	if(m_file != NULL) {
+		size_t num = fwrite(data, 1, size, m_file);
+		printf("get(%lu), write(%lu)\n", size, num);
+	}
 
 	return;
 }
@@ -34,7 +52,7 @@ int main(int argc, char **argv)
 {
 	std::string url = "http://127.0.0.1:8080/?action=stream";
 	Shdl::IStreamDownload *pdl = Shdl::IStreamDownload::Create();
-	CTestStreamDownload testDl;
+	CTestStreamDownload testDl(argv[1]);
 
 	pdl->SetCallback(&testDl, &testDl);
 	pdl->FetchStream(url);
