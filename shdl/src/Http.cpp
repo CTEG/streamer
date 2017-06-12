7e6f90269c2e0d4f6857d507fff6e9f4691e2423
残剑 <txgcwm@163.com>
2017-06-12 11:33:29 +0800

    Create Http.cpp

--- shdl/src/Http.cpp
+++ shdl/src/Http.cpp
@@ -12,8 +12,8 @@ namespace Shdl {
 
 
 #define USER_AGENT "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) " \
-					"AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 " \
-					"Mobile/10A5355d Safari/8536.25"
+		   "AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 " \
+		   "Mobile/10A5355d Safari/8536.25"
 
 CHttp::CHttp(HTTPDATACALLBACK pcbk, void *context)
 : m_cbk(pcbk)
